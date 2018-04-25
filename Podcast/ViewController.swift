9d7b65f476c3e549a5ffdd4c4ae9e9a843f781fd
Nicolas Desormiere <nicolas@insight.co>
2018-04-25 17:53:56 +1000

    init project

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Nicolas Desormiere on 25/4/18.
+//  Copyright © 2018 Nicolas Desormiere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
